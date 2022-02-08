22e044693866614bf8a7ab088efe0473f8a677d3
YotamCohen512 <72216994+YotamCohen512@users.noreply.github.com>
2022-02-08 17:39:34 +0200

    Add project files.

---
+++ QueuePractice/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Unit4.CollectionsLib;
+
+namespace QueuePractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Test.RunTests();
+        }
+        
+
+        #region homework methods
+        public static int Sum(Queue<int> queue)
+        {
+            // returns the sum of the numbers in a queue
+            // Time Complexity: O(n)
+            // n - number of values in queue
+
+            int sum = 0;
+
+            while (!queue.IsEmpty())
+            {
+                sum += queue.Remove();
+            }
+
+            return sum;
+        }
+
+        public static int RemoveNumber(Queue<int> queue, int number)
+        {
+            // removes a given number from a given queue
+            // Time Complexity: O(2n)
+            // n - number of values in queue
+
+            Queue<int> dummy = new Queue<int>();
+            int counter = 0;
+
+            while (!queue.IsEmpty())
+            {
+                if (queue.Head() == number)
+                {
+                    queue.Remove();
+                    counter++;
+                }
+
+                else
+                {
+                    dummy.Insert(queue.Remove());
+                }
+            }
+
+            Copy(queue, dummy);
+            return counter;
+        }
+
+        public static bool IsInQueue(Queue<int> queue, int number)
+        {
+            // checks if number is in queue
+            // Time Complexity: O(2n)
+            // n - number of values in queue
+
+            Queue<int> dummy = new Queue<int>();
+
+            while (!queue.IsEmpty())
+            {
+                if (queue.Head() == number)
+                {
+                    return true;
+                }
+                dummy.Insert(queue.Remove());
+            }
+
+            Copy(queue, dummy);
+            return false;
+        }
+
+        public static void RemoveDouble(Queue<int> queue)
+        {
+            // removes numbers that already appeared in queue
+            // Time Complexity: O(2n)
+            // n - number of values in queue
+
+            Queue<int> dummy = new Queue<int>();
+
+            while (!queue.IsEmpty())
+            {
+                dummy.Insert(queue.Head());
+                RemoveNumber(queue, queue.Remove());
+            }
+
+            Copy(queue, dummy);
+        }
+
+        #endregion
+
+        #region support methods
+        public static int Count(Queue<int> queue)
+        {
+            // counts the number of values in a queue
+            // Time Complexity: O(2n)
+            // n - number of values in queue
+
+            Queue<int> dummy = new Queue<int>();
+            int counter = 0;
+
+            while (!queue.IsEmpty())
+            {
+                counter++;
+                dummy.Insert(queue.Remove());
+            }
+
+            Copy(queue, dummy);
+            return counter;
+        }
+
+        public static void Copy(Queue<int> q1, Queue<int> q2)
+        {
+            // copies q2 onto q1 while ruining q2
+            // Time Complexity: O(n)
+            // n - number of values in queue
+
+            while (!q2.IsEmpty())
+            {
+                q1.Insert(q2.Remove());
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ QueuePractice/Test.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using Unit4.CollectionsLib;
+
+namespace QueuePractice
+{
+    static class Test
+    {
+        public static void RunTests()
+        {
+            Console.Write("SumTest: ");
+            PrintPassOrFailed(SumTest());
+
+            Console.Write("RemoveNumberTest: ");
+            PrintPassOrFailed(RemoveNumberTest());
+
+            Console.Write("IsInQueueTest: ");
+            PrintPassOrFailed(IsInQueueTest());
+
+            Console.Write("RemoveDoubleTest: ");
+            PrintPassOrFailed(RemoveDoubleTest());
+
+        }
+
+        private static void PrintPassOrFailed(bool isPass)
+        {
+            if (isPass)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Fail");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static bool SumTest()
+        {
+            // creates queue
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Insert(i);
+            }
+
+            // tests sum method
+            return Program.Sum(queue) == 45;
+        }
+
+        private static bool RemoveNumberTest()
+        {
+            // creates queue
+            Queue<int> queue = new Queue<int>();
+            queue.Insert(3);
+            queue.Insert(5);
+            queue.Insert(5);
+            queue.Insert(8);
+
+            // tests remove number method
+            return Program.RemoveNumber(queue, 5) == 2 && queue.Remove() == 3 && queue.Remove() == 8 && queue.IsEmpty();
+        }
+
+        private static bool IsInQueueTest()
+        {
+            // creates queue
+            Queue<int> queue = new Queue<int>();
+            queue.Insert(1);
+            queue.Insert(4);
+            queue.Insert(7);
+            queue.Insert(8);
+
+            // tests is in queue method
+            return Program.IsInQueue(queue, 4) && !Program.IsInQueue(queue, 5);
+        }
+
+        private static bool RemoveDoubleTest()
+        {
+            // creates queue
+            Queue<int> queue = new Queue<int>();
+            queue.Insert(1);
+            queue.Insert(1);
+            queue.Insert(3);
+            queue.Insert(3);
+
+            // tests remove double method
+            Program.RemoveDouble(queue);
+            return queue.Remove() == 1 && queue.Remove() == 3 && queue.IsEmpty();
+        }
+    }
+}
